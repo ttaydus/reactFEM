e5ad94ef1d4e758f9f05d208eff5db77522b0427
Tyler <tylertaydus4444@gmail.com>
2019-11-19 11:12:00 -0700

    tools set up, pet component seperate

--- src/app.js
+++ src/app.js
@@ -1,38 +1,27 @@
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, name),
-        React.createElement('h2', {}, animal),
-        React.createElement('h2', {}, breed),
-    ])
-}
+import React from 'react';
+import { render } from 'react-dom'
+import Pet from './Pet';
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h1', {}, "Adopt Me!"),
-            React.createElement(Pet, { 
-                name: 'Chubbs', 
-                animal: 'Dog', 
-                breed: 'Tasmanian Devil'
-            }),
-            React.createElement(Pet, { 
-                name: 'Mr. Bo Jangles', 
-                animal: 'Cat', 
-                breed: 'Mixed'
-            }),
-            React.createElement(Pet, 
-                { name: 'Flap', 
-                animal: 'Bird', 
-                breed: 'Exotic'
-            })
-        ]
-
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Chubbs",
+      animal: "Dog",
+      breed: "Tasmanian Devil"
+    }),
+    React.createElement(Pet, {
+      name: "Mr. Bo Jangles",
+      animal: "Cat",
+      breed: "Mixed"
+    }),
+    React.createElement(Pet, {
+      name: "Flap",
+      animal: "Bird",
+      breed: "Exotic"
+    })
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-);
+render(React.createElement(App), 
+document.getElementById("root"));
